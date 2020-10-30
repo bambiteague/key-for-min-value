07478e6ca500d7618910faa8645fa96aa5a9d0b2
bambi teague <teeegsa@gmail.com>
2020-10-30 10:31:38 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,27 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    # need to determine which key's value is the lowest
+    # collect the correct key (the one with the lowest value) and create a variable for it. 
+    # Remember that you need your method to return JUST FOR THIS KEY
+    # hash = {:blake => 500, :ashley => 2, :adam => 1}
+    key = nil
+    lowest_value = nil
+    name_hash.each do |name, num|
+        if lowest_value == nil || num < lowest_value
+            # need to reassign key and lowest value to represent the current key and value
+        key = name
+        lowest_value = num
+        end
+    end
+    key
 end
+
+# OFF LIMITS methods
+                # `#key`
+                # `#keys`
+                # `#values`
+                # `#min`
+                # `#sort`
+                # `#sort_by`
+                # `#min_by`
